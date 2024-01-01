14bb7f92850815d628d3edeecc9cb4817c8cb3b5
Thomas Tran <thomasltran42@gmail.com>
2024-01-01 02:44:02 -0500

    timeline

---
+++ src/routes/home/intro.svelte
@@ -0,0 +1,22 @@
+<body>
+    <h1>Hi! My name is Thomas Tran.</h1>
+    <p>
+        I'm an aspiring software engineer currently majoring in Computer Science
+        with a minor in Mathematics at Virginia Tech.
+    </p>
+    <p>
+        My interests revolve around software development, cloud computing, and
+        big data analytics.
+    </p>
+</body>
+
+<style>
+    * {
+        text-align: center;
+        color: #0e1111;
+    }
+
+    p {
+        font-size: 1.2em;
+    }
+</style>
