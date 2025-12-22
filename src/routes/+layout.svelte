984e58d913f2531327392163344ad4a633670768
thomasltran <thomasltran42@gmail.com>
2025-12-22 00:30:16 -0500

    landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
+	import './layout.css';
 
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    // No scripts needed for now
+</script>
+
+<div class="container mx-auto px-4 py-12 md:py-24 max-w-[693px]">
+    
+    <div class="space-y-10">
+        
+        <header class="space-y-4">
+            <h1 class="h1">
+                Hello, I'm Thomas Tran 👍
+            </h1>
+        </header>
+
+        <hr class="opacity-30" />
+
+        <article class="space-y-6 text-lg leading-relaxed">
+            
+            <p>
+                I work as a Software Engineer at <strong>Capital One</strong> for 
+                <a class="anchor" href="https://www.capitalone.com/software/products/databolt/" target="_blank">Databolt</a>.
+            </p>
+
+            <p>
+                After completing undergrad at <strong>Virginia Tech</strong> in May '25, I decided to pursue a Master's at <strong>Georgia Tech</strong> to further my studies in OS.
+            </p>
+
+            <p>
+                My college highlights include benching 225 at McComas, serving as a TA for 
+                <em>CS 3214: Computer Systems</em> in <a class="anchor" target="_blank" href="https://courses.cs.vt.edu/cs3214/fall2024/staff">Fall '24</a>/<a class="anchor" target="_blank" href="https://courses.cs.vt.edu/cs3214/spring2025/staff">Spring '25</a>, 
+                and completing the <a class="anchor" target="_blank" href="https://courses.cs.vt.edu/cs4284/spring2025/pintos">PintOS projects</a> + <a class="anchor" target="_blank" href="https://drive.google.com/file/d/1X15dgRbBwQAMscN9mra2Mpt3C22FzzQ6/view?usp=sharing">implementing POSIX-inspired multithreading</a>.
+            </p>
+
+            <p>
+                Outside of tech, I spend most of my time in the gym. Feel free to check out my blog where I record my journey to benching 315.
+            </p>
+
+        </article>
+
+    </div>
+</div>
