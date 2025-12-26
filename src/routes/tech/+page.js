238ebbf2f75b9d743f156552f0f3e770dff456a4
thomasltran <thomasltran42@gmail.com>
2025-12-26 02:35:59 -0500

    browser check

--- src/routes/tech/+page.js
+++ src/routes/tech/+page.js
@@ -1,11 +1,17 @@
 import { loadPosts, paginatePosts } from '$lib/blog.js';
+import { browser } from '$app/environment';
 
 // Enable prerendering - this will prerender /tech without query params
 export const prerender = true;
 
 export async function load({ url }) {
-    // During prerender, url.searchParams won't work, so default to page 1
-    const page = parseInt(url.searchParams.get('page') || '1', 10);
+    // During prerender, always use page 1
+    // In browser, read from query params
+    let page = 1;
+    
+    if (browser) {
+        page = parseInt(url.searchParams.get('page') || '1', 10);
+    }
     
     // Load all tech posts
     const allPosts = await loadPosts('tech');
